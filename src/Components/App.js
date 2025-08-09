1c57be7e36be158102cda8253d09c63839627996
Jairam <jsjairam01@gmail.com>
2025-08-09 12:16:02 +0530

    Setup for my website

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+const App = () => {
+  return (
+    <div>
+      {/* Your app components go here */}
+      <h1>Welcome to My App</h1>
+      <Button type="primary">Click Me</Button>;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './Components/App.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Includes Popper.js
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Font Awesome icons
+import 'antd/dist/reset.css'; // Ant Design styles (reset in AntD v5+)
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
